c3303a424c4e44f9dadc6dee72fcf11a41c8dec4
Frank Grove <fragro@gmail.com>
2016-10-17 15:58:09 -0500

    remove test titles from git repo

--- titlechainr.ocr/titles/ocr-pdf.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for pdfile in *.pdf ; do
-  pdftoppm -rx 300 -ry 300 -png "${pdfile}" "${pdfile%.*}"
-done
-
-for pngfiles in *.png ; do
-  tesseract "${pngfiles}" "${pngfiles}".out
-done
